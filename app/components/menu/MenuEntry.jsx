764536b100a58bcb6f1c8b083b42aa5bce5a08fc
Michael Braun <michaelsoftware1997@gmail.com>
2016-11-27 00:46:56 +0100

    Updated MenuEntry-component

--- app/components/menu/MenuEntry.jsx
+++ app/components/menu/MenuEntry.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import './MenuEntry.scss';
+
 export default class MenuEntry extends React.Component {
     render() {
         return(
-            <div>
-
+            <div className="menu__entry" onClick={this._onClick.bind(this)}>
+                <div className="menu__entry__icon">
+                    <i className="fa fa-floppy-o" />
+                </div>
+                <div className="menu__entry__title">
+                    {this.props.children}
+                </div>
             </div>
         );
+    }
+
+    _onClick() {
+        if(this.props.onClick)
+            this.props.onClick();
     }
 }
