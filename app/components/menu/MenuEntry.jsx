78f30454c326255f470969a273c97370d5ae3047
Michael Braun <michaelsoftware1997@gmail.com>
2016-12-05 23:01:03 +0100

    Added iconSize-property

--- app/components/menu/MenuEntry.jsx
+++ app/components/menu/MenuEntry.jsx
@@ -10,10 +10,15 @@ export default class MenuEntry extends React.Component {
             [this.props.icon]: this.props.icon
         });
 
+        let style = {};
+        if(this.props.iconSize) {
+            style.fontSize = this.props.iconSize
+        }
+
         return(
             <div className="menu__entry" onClick={this._onClick.bind(this)}>
                 <div className="menu__entry__icon">
-                    <i className={classNames} />
+                    <i className={classNames} style={style} />
                 </div>
                 <div className="menu__entry__title">
                     {this.props.children}
