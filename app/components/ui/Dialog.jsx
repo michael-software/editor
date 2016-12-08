ac6cc08b7fbeb0c0715cbabf89c0371218120fa8
Michael Braun <michaelsoftware1997@gmail.com>
2016-12-08 21:53:59 +0100

    Added Dialog-component

---
+++ app/components/ui/Dialog.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import assign from 'object-assign';
+import connectToStores from 'alt-utils/lib/connectToStores';
+
+import UiStore from '../../stores/UiStore';
+
+import LinkDialog from './dialogs/LinkDialog';
+
+@connectToStores
+export default class Dialog extends React.Component {
+
+    static getStores() {
+        return [UiStore];
+    }
+
+    static getPropsFromStores() {
+        return assign({},
+            UiStore.getState()
+        );
+    }
+
+    render() {
+        switch(this.props.dialog) {
+            case UiStore.LINK_DIALOG:
+                return (<LinkDialog />);
+            default:
+                return null;
+        }
+    }
+}
